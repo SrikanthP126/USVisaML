8453954c54f2948517b37efdf49d255fa8b004a4
SrikanthP126 <142272971+SrikanthP126@users.noreply.github.com>
2024-11-13 00:04:30 -0500

    Update bash_1.sh

--- bash_1.sh
+++ bash_1.sh
@@ -1,24 +1,36 @@
-# Get the list of files in /NLD/ directory
-file_list=$(curl -s -X 'GET' "https://func-ark-prod-aze2.azurewebsites.net/api/listfiles?directory=NLD" \
+#!/bin/bash
+
+# Define the destination directory and log file
+DEST_DIR="/ark/landing_zone/idf"
+LOG_FILE="/home/pj72693/IDF_Download_LOG/download_log_$(date '+%Y%m%d%H%M%S').log"
+
+# Fetch the list of files from the Azure function and parse JSON to extract file names
+file_list=$(curl -s -X 'GET' "https://func-ark-prod-aze2.azurewebsites.net/api/getfile?name=idf/submission" \
     -H 'accept: application/json' \
-    --cert /home/pj72693/azureappreg-ark-deployment-pipeline-qa.pncint.net.pem \
-    --pass 'Rydd#17$')
+    --cert /ark/landing_zone/cert/azureappreg-ark-onprem-prod.pncint.net.pem \
+    --pass 'Aopcc#24$')
 
-# Convert JSON response to an array of file names (assuming JSON array response)
-file_list=($(echo $file_list | jq -r '.[]')) # Requires 'jq' to parse JSON
+# Convert JSON list of files into an array
+file_list=($(echo "$file_list" | jq -r '.[]'))
 
+# Loop through each file and download
 for filename in "${file_list[@]}"; do
     echo "Downloading file: $filename" >> $LOG_FILE
-    curl -v -X 'GET' "https://func-ark-prod-aze2.azurewebsites.net/api/getfile?name=download/NLD/${filename}" \
+    
+    # Download the file using the HTTP function
+    curl -v -X 'GET' "https://func-ark-prod-aze2.azurewebsites.net/api/getfile?name=idf/submission/$filename" \
         -H 'accept: application/json' \
         --output "${DEST_DIR}/${filename}" \
-        --cert /home/pj72693/azureappreg-ark-deployment-pipeline-qa.pncint.net.pem \
-        --pass 'Rydd#17$'
-
+        --cert /ark/landing_zone/cert/azureappreg-ark-onprem-prod.pncint.net.pem \
+        --pass 'Aopcc#24$'
+    
     # Check if download was successful
     if [ $? -eq 0 ]; then
         echo "Downloaded successfully: $filename" >> $LOG_FILE
     else
         echo "Error downloading file: $filename" >> $LOG_FILE
     fi
+    echo "----------------------------------------" >> $LOG_FILE
 done
+
+echo "All files downloaded from Azure successfully" >> $LOG_FILE
